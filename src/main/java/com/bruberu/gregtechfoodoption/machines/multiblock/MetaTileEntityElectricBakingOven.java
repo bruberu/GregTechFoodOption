a0e2f8819494ea4824f5a717aa9e2e879ced4c5d
bruberu <80226372+bruberu@users.noreply.github.com>
2021-06-01 17:01:09 -0500

    Fix assorted issues with the EBO

--- src/main/java/com/bruberu/gregtechfoodoption/machines/multiblock/MetaTileEntityElectricBakingOven.java
+++ src/main/java/com/bruberu/gregtechfoodoption/machines/multiblock/MetaTileEntityElectricBakingOven.java
@@ -3,62 +3,36 @@ package com.bruberu.gregtechfoodoption.machines.multiblock;
 import codechicken.lib.render.CCRenderState;
 import codechicken.lib.render.pipeline.IVertexOperation;
 import codechicken.lib.vec.Matrix4;
-import com.bruberu.gregtechfoodoption.block.GTFOBlockCasing;
-import com.bruberu.gregtechfoodoption.block.GTFOMetaBlocks;
 import com.bruberu.gregtechfoodoption.client.GTFOClientHandler;
 import com.bruberu.gregtechfoodoption.recipe.GTFORecipeMaps;
-import com.bruberu.gregtechfoodoption.recipe.multiblock.BakingOvenRecipe;
 import gregicadditions.capabilities.impl.GAMultiblockRecipeLogic;
 import gregicadditions.capabilities.impl.GARecipeMapMultiblockController;
 import gregicadditions.item.GAMetaBlocks;
 import gregicadditions.item.GATransparentCasing;
-import gregtech.api.capability.impl.ItemHandlerProxy;
-import gregtech.api.gui.GuiTextures;
-import gregtech.api.gui.ModularUI;
 import gregtech.api.gui.Widget;
-import gregtech.api.gui.widgets.ProgressWidget;
-import gregtech.api.gui.widgets.SlotWidget;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.MetaTileEntityHolder;
 import gregtech.api.metatileentity.multiblock.IMultiblockPart;
 import gregtech.api.metatileentity.multiblock.MultiblockAbility;
-import gregtech.api.metatileentity.multiblock.MultiblockControllerBase;
 import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
 import gregtech.api.multiblock.BlockPattern;
 import gregtech.api.multiblock.FactoryBlockPattern;
 import gregtech.api.render.ICubeRenderer;
-import gregtech.api.unification.OreDictUnifier;
-import gregtech.api.unification.material.Materials;
-import gregtech.api.unification.ore.OrePrefix;
 import gregtech.common.blocks.MetaBlocks;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.resources.I18n;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
 import net.minecraft.network.PacketBuffer;
-import net.minecraft.tileentity.TileEntityFurnace;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.*;
 import net.minecraft.world.World;
-import net.minecraftforge.common.util.Constants;
-import net.minecraftforge.items.IItemHandlerModifiable;
-import net.minecraftforge.items.ItemStackHandler;
 
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import javax.xml.soap.Text;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static gregtech.api.gui.widgets.AdvancedTextWidget.withButton;
 import static gregtech.api.unification.material.Materials.BismuthBronze;
-import static gregtech.api.unification.material.Materials.Iron;
-import static gregtech.api.util.InventoryUtils.simulateItemStackMerge;
+import static gregtech.api.unification.material.Materials.Steel;
 
 public class MetaTileEntityElectricBakingOven extends GARecipeMapMultiblockController {
 
@@ -100,7 +74,7 @@ public class MetaTileEntityElectricBakingOven extends GARecipeMapMultiblockContr
             temp -= 5;
             return;
         }
-        if(temperatureEnergyCost(this.temp + 5) <= this.getEnergyContainer().getInputVoltage() * this.getEnergyContainer().getInputAmperage()) {
+        if(temperatureEnergyCost(this.temp + 5) <= this.getEnergyContainer().getInputVoltage() * this.getEnergyContainer().getInputAmperage() && hasEnoughEnergy) {
             temp += 5;
         }
         else {
@@ -131,27 +105,38 @@ public class MetaTileEntityElectricBakingOven extends GARecipeMapMultiblockContr
             textList.add(new TextComponentTranslation("gregtechfoodoption.multiblock.electric_baking_oven.tooltip.1", temp));
             textList.add(new TextComponentTranslation("gregtechfoodoption.multiblock.electric_baking_oven.tooltip.4", temperatureEnergyCost(temp)));
 
-            if (!canAchieveTargetTemp)
-                textList.add(new TextComponentTranslation("gregtechfoodoption.multiblock.electric_baking_oven.tooltip.2")
-                        .setStyle(new Style().setColor(TextFormatting.RED)));
-            if (!hasEnoughEnergy)
-                textList.add(new TextComponentTranslation("gregtech.multiblock.not_enough_energy")
-                        .setStyle(new Style().setColor(TextFormatting.RED)));
-
             ITextComponent buttonText = new TextComponentTranslation("gregtechfoodoption.multiblock.electric_baking_oven.tooltip.3");
             buttonText.appendText(" ");
             buttonText.appendSibling(withButton(new TextComponentString("[-]"), "sub"));
             buttonText.appendText(" ");
             buttonText.appendSibling(withButton(new TextComponentString("[+]"), "add"));
             textList.add(buttonText);
+
+            textList.add(new TextComponentTranslation("gregtechfoodoption.multiblock.electric_baking_oven.tooltip.5", targetTemp));
+
+
+            if (!canAchieveTargetTemp && hasEnoughEnergy)
+                textList.add(new TextComponentTranslation("gregtechfoodoption.multiblock.electric_baking_oven.tooltip.2")
+                        .setStyle(new Style().setColor(TextFormatting.RED)));
+            if (!hasEnoughEnergy)
+                textList.add(new TextComponentTranslation("gregtech.multiblock.not_enough_energy")
+                        .setStyle(new Style().setColor(TextFormatting.RED)));
+
         }
+        else
+            super.addDisplayText(textList);
+
     }
+
+
 
     @Override
     protected void handleDisplayClick(String componentData, Widget.ClickData clickData) {
         super.handleDisplayClick(componentData, clickData);
         int modifier = componentData.equals("add") ? 1 : -1;
         targetTemp += 5 * modifier;
+        if(targetTemp < 300)
+            targetTemp = 300;
     }
 
     protected IBlockState getCasingState() {
@@ -159,7 +144,7 @@ public class MetaTileEntityElectricBakingOven extends GARecipeMapMultiblockContr
     }
 
     protected IBlockState getFrameState() {
-        return MetaBlocks.FRAMES.get(Iron).getDefaultState();
+        return MetaBlocks.FRAMES.get(Steel).getDefaultState();
     }
 
     @Override
@@ -179,11 +164,11 @@ public class MetaTileEntityElectricBakingOven extends GARecipeMapMultiblockContr
                 .aisle("XXXX", "XXXX", "XXXX", "#XX#")
                 .aisle("XXXX", "XFFX", "X##X", "#XX#")
                 .aisle("XXXX", "XFFX", "X##X", "#XX#")
-                .aisle("XXXX", "Y##X", "X##X", "#XX#")
+                .aisle("XXXX", "YGGX", "XGGX", "#XX#")
                 .where('X', statePredicate(getCasingState()).or(abilityPartPredicate(ALLOWED_ABILITIES)))
                 .where('F', statePredicate(getFrameState()))
                 .where('G', statePredicate(GAMetaBlocks.TRANSPARENT_CASING.getState(GATransparentCasing.CasingType.REINFORCED_GLASS)))
-                .where('#', isAirPredicate())
+                .where('#', (tile) -> true)
                 .where('Y', selfPredicate())
                 .build();
 
@@ -203,6 +188,43 @@ public class MetaTileEntityElectricBakingOven extends GARecipeMapMultiblockContr
         if (EUt <= 8)
             return 300;
         return (int) (Math.log(EUt) * 100) + 100;
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound data) {
+        super.writeToNBT(data);
+        data.setInteger("temp", this.temp);
+        data.setInteger("targetTemp", this.targetTemp);
+        data.setBoolean("canAchieveTargetTemp", this.canAchieveTargetTemp);
+        data.setBoolean("hasEnoughEnergy", this.hasEnoughEnergy);
+        return data;
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound data) {
+        super.readFromNBT(data);
+        this.temp = data.getInteger("temp");
+        this.targetTemp = data.getInteger("targetTemp");
+        this.canAchieveTargetTemp = data.getBoolean("canAchieveTargetTemp");
+        this.hasEnoughEnergy = data.getBoolean("hasEnoughEnergy");
+    }
+
+    @Override
+    public void writeInitialSyncData(PacketBuffer buf) {
+        super.writeInitialSyncData(buf);
+        buf.writeInt(this.temp);
+        buf.writeInt(this.targetTemp);
+        buf.writeBoolean(this.canAchieveTargetTemp);
+        buf.writeBoolean(this.hasEnoughEnergy);
+    }
+
+    @Override
+    public void receiveInitialSyncData(PacketBuffer buf) {
+        super.receiveInitialSyncData(buf);
+        this.temp = buf.readInt();
+        this.targetTemp = buf.readInt();
+        this.canAchieveTargetTemp = buf.readBoolean();
+        this.hasEnoughEnergy = buf.readBoolean();
     }
 
     private class ElectricBakingOvenLogic extends GAMultiblockRecipeLogic {
