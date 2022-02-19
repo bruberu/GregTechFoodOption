7eb88d27cf5105e44f05347cc400d94884388c74
Bioastroiner <naderi81eco@gmail.com>
2022-02-19 07:45:45 +0330

    make Recipes actually respect the energy of their unit base!! like any other tools

--- src/main/java/gregtechfoodoption/recipe/GTFORecipeHandler.java
+++ src/main/java/gregtechfoodoption/recipe/GTFORecipeHandler.java
@@ -1,5 +1,6 @@
 package gregtechfoodoption.recipe;
 
+import gregtech.api.capability.GregtechCapabilities;
 import gregtech.api.recipes.ModHandler;
 import gregtech.api.recipes.RecipeMaps;
 import gregtech.api.unification.material.MarkerMaterials;
@@ -9,8 +10,10 @@ import gregtech.api.unification.material.properties.PropertyKey;
 import gregtech.api.unification.material.properties.ToolProperty;
 import gregtech.api.unification.ore.OrePrefix;
 import gregtech.api.unification.stack.UnificationEntry;
-import gregtech.common.items.MetaItems;
 import gregtechfoodoption.item.GTFOMetaItem;
+import net.minecraft.item.ItemStack;
+
+import static gregtech.common.items.MetaItems.POWER_UNIT_HV;
 
 public class GTFORecipeHandler {
 
@@ -32,16 +35,21 @@ public class GTFORecipeHandler {
 
     private static void processSword(OrePrefix prefix, Material material, ToolProperty property) {
         if (property.getToolDurability() > 0) {
-            RecipeMaps.ASSEMBLER_RECIPES.recipeBuilder().EUt(480).duration(2500)
-                    .input(prefix, material)
-                    .input(OrePrefix.stick, material, 2)
-                    .input(OrePrefix.plate, material)
-                    .input(OrePrefix.circuit, MarkerMaterials.Tier.Extreme, 2)
-                    .inputs(MetaItems.POWER_UNIT_HV.getStackForm())
-                    .input(OrePrefix.wireGtDouble, Materials.MercuryBariumCalciumCuprate, 8)
-                    .fluidInputs(Materials.SolderingAlloy.getFluid(144 * 20))
-                    .outputs(GTFOMetaItem.BUTCHERY_KNIFE_HV.getStackForm(material))
-                    .buildAndRegister();
+            ItemStack[] power_units = {POWER_UNIT_HV.getMaxChargeOverrideStack(1800000L), POWER_UNIT_HV.getMaxChargeOverrideStack(1600000L), POWER_UNIT_HV.getMaxChargeOverrideStack(1200000L), POWER_UNIT_HV.getMaxChargeOverrideStack(6400000L)};
+            for (ItemStack power_unit : power_units) {
+                ItemStack output = GTFOMetaItem.BUTCHERY_KNIFE_HV.getMaxChargeOverrideStack(material, power_unit.getCapability(GregtechCapabilities.CAPABILITY_ELECTRIC_ITEM, null).getMaxCharge());
+                RecipeMaps.ASSEMBLER_RECIPES.recipeBuilder().EUt(480).duration(250)
+                        .input(prefix, material)
+                        .input(OrePrefix.stick, material, 2)
+                        .input(OrePrefix.plate, material)
+                        .input(OrePrefix.circuit, MarkerMaterials.Tier.Extreme, 2)
+                        .inputs(power_unit)
+                        .input(OrePrefix.wireGtDouble, Materials.MercuryBariumCalciumCuprate, 4)
+                        .fluidInputs(Materials.SolderingAlloy.getFluid(144 * 10))
+                        .outputs(output)
+                        .buildAndRegister();
+            }
+
         }
 
     }
