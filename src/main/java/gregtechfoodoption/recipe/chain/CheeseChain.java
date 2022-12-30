7a4b34b7c7dc0e8eb1ae06d37d6b91e27e473cc3
bruberu <80226372+bruberu@users.noreply.github.com>
2022-12-30 15:09:38 -0600

    Biomass recipes from GTFO crops

--- src/main/java/gregtechfoodoption/recipe/chain/CheeseChain.java
+++ src/main/java/gregtechfoodoption/recipe/chain/CheeseChain.java
@@ -102,7 +102,7 @@ public class CheeseChain {
                 .fluidOutputs(HeatedRicottaStarter.getFluid(1000))
                 .buildAndRegister();
         MIXER_RECIPES.recipeBuilder().EUt(12).duration(40)
-                .fluidInputs(Milk.getFluid(144), LemonExtract.getFluid(2))
+                .fluidInputs(Milk.getFluid(144), LemonExtract.getFluid(10))
                 .fluidOutputs(AcidicMilkSolution.getFluid(144))
                 .buildAndRegister();
         MIXER_RECIPES.recipeBuilder().EUt(12).duration(40)
