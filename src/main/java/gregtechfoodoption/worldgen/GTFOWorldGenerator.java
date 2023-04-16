01c16e62a9b9816dbf8f7a4291215cf6b8110f27
bruberu <80226372+bruberu@users.noreply.github.com>
2023-04-16 14:09:43 -0500

    Fix tree config

--- src/main/java/gregtechfoodoption/worldgen/GTFOWorldGenerator.java
+++ src/main/java/gregtechfoodoption/worldgen/GTFOWorldGenerator.java
@@ -16,11 +16,11 @@ public class GTFOWorldGenerator implements IWorldGenerator {
 
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator iChunkGenerator, IChunkProvider iChunkProvider) {
-        if (iChunkGenerator instanceof ChunkGeneratorFlat || !GTFOConfig.enableGTFOTrees)
-            return; // It's a superflat world, don't generate trees.
-        for (GTFOTree tree : GTFOTree.TREES) {
-            tree.setWorld(world);
-            tree.getWorldGenInstance().generateInChunk(world, random, chunkX, chunkZ);
+        if (!(iChunkGenerator instanceof ChunkGeneratorFlat) && GTFOConfig.enableGTFOTrees) {
+            for (GTFOTree tree : GTFOTree.TREES) {
+                tree.setWorld(world);
+                tree.getWorldGenInstance().generateInChunk(world, random, chunkX, chunkZ);
+            }
         }
     }
 }
