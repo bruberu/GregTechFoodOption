93317dcc69e2f36884cbb0c1993d3f0f7f4f35d4
oliwier509 <oliwier5009.2003@wp.pl>
2026-07-08 09:01:18 +0200

    sprinkler fire suppression system logic

---
+++ src/main/java/gregtechfoodoption/utils/GTFOFireSuppressantProperty.java
@@ -0,0 +1,14 @@
+package gregtechfoodoption.utils;
+
+import gregtech.api.unification.material.properties.IMaterialProperty;
+import gregtech.api.unification.material.properties.MaterialProperties;
+import gregtech.api.unification.material.properties.PropertyKey;
+
+public class GTFOFireSuppressantProperty implements IMaterialProperty {
+
+    public static final PropertyKey<GTFOFireSuppressantProperty> FIRE_SUPPRESSANT =
+            new PropertyKey<>("fire_suppressant", GTFOFireSuppressantProperty.class);
+
+    @Override
+    public void verifyProperty(MaterialProperties properties) {}
+}
