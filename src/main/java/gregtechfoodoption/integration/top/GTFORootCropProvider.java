4a54c2f2d2bc6e76896c859740de11d10071ff3d
ko_no <90126004+MrKono@users.noreply.github.com>
2026-07-22 20:20:53 +0900

    use `{*key*} instead of `I18n.format()`

--- src/main/java/gregtechfoodoption/integration/top/GTFORootCropProvider.java
+++ src/main/java/gregtechfoodoption/integration/top/GTFORootCropProvider.java
@@ -1,42 +1,43 @@
 package gregtechfoodoption.integration.top;
 
-import gregtechfoodoption.GTFOValues;
-import gregtechfoodoption.block.GTFORootCrop;
-import mcjty.theoneprobe.api.*;
+import javax.annotation.Nonnull;
+
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
-import javax.annotation.Nonnull;
+import gregtechfoodoption.GTFOValues;
+import gregtechfoodoption.block.GTFORootCrop;
+import mcjty.theoneprobe.api.*;
 
 public class GTFORootCropProvider implements IProbeInfoProvider {
+
     @Override
     public String getID() {
         return String.format("%s:root_crop_provider", GTFOValues.MODID);
     }
 
     @Override
-    public void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world, @Nonnull IBlockState blockState, IProbeHitData data) {
+    public void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world,
+                             @Nonnull IBlockState blockState, IProbeHitData data) {
         if (blockState.getBlock() instanceof GTFORootCrop) {
             GTFORootCrop crop = (GTFORootCrop) blockState.getBlock();
 
             if (crop.cropHarvestable(blockState)) {
                 probeInfo.text(TextStyleClass.OK +
-                        I18n.format("gregtechfoodoption.top.crop_harvestable"));
+                        "{*gregtechfoodoption.top.crop_harvestable*}");
             } else {
                 probeInfo.text(TextStyleClass.ERROR +
-                        I18n.format("gregtechfoodoption.top.crop_unharvestable"));
+                        "{*gregtechfoodoption.top.crop_unharvestable*}");
             }
 
             if (crop.seedHarvestable(blockState)) {
                 probeInfo.text(TextStyleClass.OK +
-                        I18n.format("gregtechfoodoption.top.seed_harvestable"));
+                        "{*gregtechfoodoption.top.seed_harvestable*}");
             } else {
                 probeInfo.text(TextStyleClass.ERROR +
-                        I18n.format("gregtechfoodoption.top.seed_unharvestable"));
+                        "{*gregtechfoodoption.top.seed_unharvestable*}");
             }
         }
     }
-
 }
